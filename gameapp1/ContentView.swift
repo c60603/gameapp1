f0b1d44e8c3d01c0e7efda347d71036e8db72039
lin1999 <menghan1117@gmail.com>
2021-03-08 13:19:26 +0800

    Initial Commit

---
+++ gameapp1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gameapp1
+//
+//  Created by User11 on 2021/3/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gameapp1/gameapp1App.swift
@@ -0,0 +1,17 @@
+//
+//  gameapp1App.swift
+//  gameapp1
+//
+//  Created by User11 on 2021/3/8.
+//
+
+import SwiftUI
+
+@main
+struct gameapp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
